2beefd5582c66d01d8e85a606ce55c608da50f96
GooseThings <levi.gates@gmail.com>
2026-04-01 21:28:49 -0400

    Update header comment in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-ASL3 rpt.conf Web Editor
+N8GMZ ASL3 rpt.conf Web Editor
 A simple web interface for editing AllStarLink 3 rpt.conf settings
 and restarting Asterisk.
 """
